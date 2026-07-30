00d27e57b075f0383eea2c4ca65a5ccf5f8cd2da
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-07-30 22:16:43 +0600

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization code.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
-// Firebase configuration will be added here.
+// Import the functions you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb_5uvucPU675nVXX5wk-As3BOt1K7rB8",
+  authDomain: "zr-business-platform-67b07.firebaseapp.com",
+  projectId: "zr-business-platform-67b07",
+  storageBucket: "zr-business-platform-67b07.firebasestorage.app",
+  messagingSenderId: "127810584577",
+  appId: "1:127810584577:web:3355f717d4f10467124000"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected Successfully");
