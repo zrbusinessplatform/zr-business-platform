f90674104a162f4bb58a993504f42ef8bf147fa2
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-07-31 19:39:27 +0600

    Update firebase configuration

--- firebase.js
+++ firebase.js
@@ -1,20 +1,5 @@
-// Import the functions you need
+// Firebase App
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCb_5uvucPU675nVXX5wk-As3BOt1K7rB8",
-  authDomain: "zr-business-platform-67b07.firebaseapp.com",
-  projectId: "zr-business-platform-67b07",
-  storageBucket: "zr-business-platform-67b07.firebasestorage.app",
-  messagingSenderId: "127810584577",
-  appId: "1:127810584577:web:3355f717d4f10467124000"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-console.log("Firebase Connected Successfully");
 
 // Authentication
 import {
@@ -26,8 +11,18 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCb_5uvucPU675nVXX5wk-As3BOt1K7rB8",
+  authDomain: "zr-business-platform-67b07.firebaseapp.com",
+  projectId: "zr-business-platform-67b07",
+  storageBucket: "zr-business-platform-67b07.firebasestorage.app",
+  messagingSenderId: "127810584577",
+  appId: "1:127810584577:web:3355f717d4f10467124000"
+};
+
+const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Export
 export { app, auth, db };
