d6ebfbc977eb4ec94b5df583997d43d54100fc7c
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-07-31 17:10:02 +0600

    Reset auth module

--- auth.js
+++ auth.js
@@ -1,8 +1,3 @@
 import { auth } from "./firebase.js";
 
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-console.log("Auth System Ready");
+console.log("Auth Loaded Successfully");
