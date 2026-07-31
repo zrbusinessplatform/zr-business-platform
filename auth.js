804df41b562561aceb1a993f4f11b5d7c65bdf60
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-07-31 19:53:32 +0600

    Add Firebase register function

--- auth.js
+++ auth.js
@@ -9,3 +9,35 @@ if (registerBtn) {
     console.log("Register button clicked");
   });
 }
+
+import { createUserWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const fullname = document.getElementById("fullname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const message = document.getElementById("message");
+
+if (registerBtn) {
+  registerBtn.addEventListener("click", async () => {
+
+    if (password.value !== confirmPassword.value) {
+      message.innerText = "Passwords do not match!";
+      return;
+    }
+
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      );
+
+      message.innerText = "Registration Successful!";
+    } catch (error) {
+      message.innerText = error.message;
+    }
+
+  });
+}
