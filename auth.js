4fc464e05d1d9b4906dbe84a094bb4dcac83b9fb
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-07-31 19:49:04 +0600

    Add register button event

--- auth.js
+++ auth.js
@@ -1,3 +1,11 @@
 import { auth } from "./firebase.js";
 
 console.log("Authentication Ready");
+
+const registerBtn = document.getElementById("registerBtn");
+
+if (registerBtn) {
+  registerBtn.addEventListener("click", () => {
+    console.log("Register button clicked");
+  });
+}
