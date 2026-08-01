920f03f8c28fc631a38657bfc73a49ac0b855d6c
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-08-01 13:31:44 +0600

    Load users from Firestore and handle errors
    Fetch users from Firestore and display in table.

--- users.js
+++ users.js
@@ -1,1 +1,47 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const tableBody = document.getElementById("userTableBody");
+
+async function loadUsers() {
+
+  tableBody.innerHTML = "<tr><td colspan='5'>Loading...</td></tr>";
+
+  try {
+
+    const querySnapshot = await getDocs(collection(db, "users"));
+
+    tableBody.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+
+      const user = doc.data();
+
+      tableBody.innerHTML += `
+        <tr>
+          <td>${user.fullName || "-"}</td>
+          <td>${user.email || "-"}</td>
+          <td>${user.role || "-"}</td>
+          <td>${user.status || "-"}</td>
+          <td>Edit</td>
+        </tr>
+      `;
+
+    });
+
+  } catch (error) {
+
+    tableBody.innerHTML =
+      "<tr><td colspan='5'>Failed to load users.</td></tr>";
+
+    console.error(error);
+
+  }
+
+}
+
+loadUsers();
