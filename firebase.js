33dc451913dbf9fdbf9e0a0d17befe82a489c2eb
zrbusinessplatform <zrbusinessplatform@gmail.com>
2026-08-01 10:15:23 +0600

    Prepare Firestore helper exports

--- firebase.js
+++ firebase.js
@@ -6,9 +6,11 @@ import {
   getAuth
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-// Firestore
 import {
-  getFirestore
+  getFirestore,
+  doc,
+  setDoc,
+  serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -25,4 +27,11 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+export {
+  app,
+  auth,
+  db,
+  doc,
+  setDoc,
+  serverTimestamp
+};
